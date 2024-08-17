2552c7def259328b5d17813af412129e40f9c68e
Mari Lopes <marilce@lesteti.com.br>
2024-08-17 20:01:30 -0300

    Iniciando projeto Spring Boot Clinica

---
+++ src/main/java/clinica/api/ApiSprintBootClinApplication.java
@@ -0,0 +1,13 @@
+package clinica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSprintBootClinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSprintBootClinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clinica/api/ApiSprintBootClinApplicationTests.java
@@ -0,0 +1,13 @@
+package clinica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSprintBootClinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

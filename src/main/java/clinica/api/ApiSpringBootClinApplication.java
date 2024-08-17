9c02cfc5560fcc9ef16d29bf8ad939349193b4d7
Matheus Barreto <matheusbarreto.dev@gmail.com>
2024-08-17 20:25:35 -0300

    Retirando a maldição

--- src/main/java/clinica/api/ApiSpringBootClinApplication.java
+++ src/main/java/clinica/api/ApiSpringBootClinApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiSprintBootClinApplication {
+public class ApiSpringBootClinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiSprintBootClinApplication.class, args);
+		SpringApplication.run(ApiSpringBootClinApplication.class, args);
 	}
 
 }

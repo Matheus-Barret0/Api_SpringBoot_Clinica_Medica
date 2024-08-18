ca9254bbfc5dc53e9842098de70fbb233a62444a
Matheus Barreto <matheusbarreto.dev@gmail.com>
2024-08-18 16:33:33 -0300

    Adicionando class e interfaces para interação com o banco de dados. Utilizando Lombok para simplificar

--- src/main/java/clinica/api/controller/MedicoController.java
+++ src/main/java/clinica/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package clinica.api.controller;
 
 import clinica.api.medico.DadosCadastroMedico;
+import clinica.api.medico.Medico;
+import clinica.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
     //DTO (DATA TRANSFER OBJECT)
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-
+        repository.save(new Medico(dados));
     }
 }
